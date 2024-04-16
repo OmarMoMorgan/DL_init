d58d2e82420b9c966d6afd0f3b737dad0c0aacf8
OmarMoMorgan <s-omar.morgan@zewailcity.edu.eg>
2024-04-16 13:56:18 +0200

    minor edit

--- models/__init__.py
+++ models/__init__.py
@@ -2,13 +2,13 @@ from .ResNetA import ResNetA
 from .plainNet import plainNet
 from .sub_CNN import sub_CNN
 
-def build_model(model_name,config):
+def build_model(model_name,config,groups):
     if model_name =='resnet':
         model = ResNetA(config)
     elif model_name == 'plain_net': 
         model = plainNet(config)
     elif model_name == 'sub_CNN':
-        model = sub_CNN(config);
+        model = sub_CNN(config,groups);
     
     return model
     
